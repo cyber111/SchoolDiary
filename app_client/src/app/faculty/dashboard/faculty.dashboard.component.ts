4868ab064ca65854c737ae4519cc9d2b55c90762
cyber111 <pjpratikjain7@gmail.com>
2019-12-28 23:05:50 +0530

    few updates

--- app_client/src/app/faculty/dashboard/faculty.dashboard.component.ts
+++ app_client/src/app/faculty/dashboard/faculty.dashboard.component.ts
@@ -1,14 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+
 
 @Component({
     selector: 'app-faculty-dashboard',
-    templateUrl: 'faculty.dashboard.component.html',
+    templateUrl:'faculty.dashboard.component',
     styleUrls:['faculty.dashboard.component.css']
 })
 
 export class FacultyDashboardComponent implements OnInit {
+
+
     constructor() { }
 
-    
+
+
     ngOnInit() { }
 }
