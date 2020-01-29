4e65d08dbb5393aec231f05569aabc0116089b01
cyber111 <pjpratikjain7@gmail.com>
2020-01-29 12:56:55 +0530

    sql backupp taken

--- mysql_backup.sql
+++ mysql_backup.sql
@@ -108,7 +108,7 @@ CREATE TABLE `faculty` (
   KEY `FK_UID_FACULTY` (`UID`),
   CONSTRAINT `FK_F_CID` FOREIGN KEY (`CID`) REFERENCES `classes` (`CID`),
   CONSTRAINT `FK_UID_FACULTY` FOREIGN KEY (`UID`) REFERENCES `user` (`UID`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -132,7 +132,7 @@ CREATE TABLE `notice` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `notice_text` varchar(10000) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -141,7 +141,7 @@ CREATE TABLE `notice` (
 
 LOCK TABLES `notice` WRITE;
 /*!40000 ALTER TABLE `notice` DISABLE KEYS */;
-INSERT INTO `notice` VALUES (1,'Pratik where id = 2'),(2,'Pratik where id = 2'),(3,'Pratik where id = 2'),(4,'Pratik where id = 2'),(5,'Pratik where id = 2'),(6,'Pratik where id = 2'),(7,'Pratik where id = 2'),(8,'Pratik where id = 2'),(9,'Pratik Jain');
+INSERT INTO `notice` VALUES (1,'Holiday ugu where id = 3'),(2,'Holiday ugu where id = 3'),(3,'Holiday ugu where id = 3'),(4,'Holiday');
 /*!40000 ALTER TABLE `notice` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -187,7 +187,7 @@ CREATE TABLE `parent` (
   `PRELATION` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`PID`),
   KEY `FK_S_ID` (`S_ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -196,8 +196,90 @@ CREATE TABLE `parent` (
 
 LOCK TABLES `parent` WRITE;
 /*!40000 ALTER TABLE `parent` DISABLE KEYS */;
-INSERT INTO `parent` VALUES (1,1,'PRASHANT','JAIN',8745157458,'prashant@gmail.com','BULDHANA','FATHER'),(4,10,'SANJAY','SANCHETI',8745157458,'sanjay@gmail.com','AURANGABAD','FATHER');
+INSERT INTO `parent` VALUES (1,1,'PRASHANT','JAIN',8745157458,'prashant@gmail.com','BULDHANA','FATHER'),(4,10,'SANJAY','SANCHETI',8745157458,'sanjay@gmail.com','AURANGABAD','FATHER'),(10,5,'Bob','Patil',8830734900,'bp@gmail.com','Pune','FATHER'),(11,4,'PRABHAKAR','LAVHANE',8830497800,'pl@gmail.com','Pune','FATHER'),(12,5,'PRAVIN','JOSHI',9763984562,'pravinjoshi@gmail.com','Buldana','Father'),(13,6,'SHAM','JOSHI',8877451245,'sham@gmail.com','pune','FATHER'),(14,7,'RAHUL','PATIL',4567891234,'RAHUKPATIL@GMAIL.COM','PUNE','FATHER'),(15,8,'DILIP','POTODE',8754623145,'dilippotode@gmail.com','Amaravati','FATHER'),(17,21,'RAJESH','JAIN',976398875,'rajjain@gmail.com','Pune','Father'),(18,21,'ABHAY','PATIL',9789456897,'abhay@gmail.com','PUNE','FATHER');
 /*!40000 ALTER TABLE `parent` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `questions`
+--
+
+DROP TABLE IF EXISTS `questions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `questions` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `test_id` int(11) DEFAULT NULL,
+  `question` varchar(500) DEFAULT NULL,
+  `answer` varchar(500) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `test_id` (`test_id`),
+  CONSTRAINT `questions_ibfk_1` FOREIGN KEY (`test_id`) REFERENCES `tests` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `questions`
+--
+
+LOCK TABLES `questions` WRITE;
+/*!40000 ALTER TABLE `questions` DISABLE KEYS */;
+INSERT INTO `questions` VALUES (1,1,'5 + 5 = ?','10'),(2,1,'6 + 6 = ?','12'),(3,1,'7++=?','14'),(4,1,'8+8=?','16'),(5,2,'5+5 = ?','10'),(6,3,'Explain the human Digestive system','The human digestive system consists of the gastrointestinal tract plus the accessory organs of digestion (the tongue, salivary glands, pancreas, liver, and gallbladder). Digestion involves the breakdown of food into smaller and smaller components, until they can be absorbed and assimilated into the body.'),(7,3,'Where Digestive Starts ?','Mouth'),(8,4,'fdxgchvjbknlm ?','Prat');
+/*!40000 ALTER TABLE `questions` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `remark`
+--
+
+DROP TABLE IF EXISTS `remark`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `remark` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `remark` varchar(500) DEFAULT NULL,
+  `sid` int(11) DEFAULT NULL,
+  `date` date DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `sid` (`sid`),
+  CONSTRAINT `remark_ibfk_1` FOREIGN KEY (`sid`) REFERENCES `student` (`S_ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `remark`
+--
+
+LOCK TABLES `remark` WRITE;
+/*!40000 ALTER TABLE `remark` DISABLE KEYS */;
+INSERT INTO `remark` VALUES (1,'asasadasd',5,'2020-01-10'),(2,'Good',5,'2020-01-02'),(3,'Bad',8,'2020-01-17'),(4,'Nice',8,'2020-01-25'),(5,'Good',4,'2020-01-16'),(6,'Need to improve Math',8,'2020-01-08'),(7,'Bad',5,'2020-01-04'),(8,'Bad',4,'2020-01-04'),(9,'Verry Good',4,'2020-01-09'),(10,'Good',7,'2020-01-02'),(11,' Bad',6,'2020-01-10'),(12,'Baddddddddddddd',4,'2020-01-15');
+/*!40000 ALTER TABLE `remark` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `schedules`
+--
+
+DROP TABLE IF EXISTS `schedules`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `schedules` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `date` date DEFAULT NULL,
+  `details` varchar(500) DEFAULT NULL,
+  `day` varchar(15) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `schedules`
+--
+
+LOCK TABLES `schedules` WRITE;
+/*!40000 ALTER TABLE `schedules` DISABLE KEYS */;
+INSERT INTO `schedules` VALUES (1,'2020-01-16','Hellol','Wednesday'),(2,'2020-01-22','123','Thursday'),(3,'2020-01-15','aa','Tuesday'),(4,'2020-01-21','Holiday','Friday'),(5,'2020-01-16','2 hrs ','Tuesday'),(6,'2020-01-25','Holiday','Saturday');
+/*!40000 ALTER TABLE `schedules` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -233,7 +315,7 @@ CREATE TABLE `student` (
   CONSTRAINT `FK_CID` FOREIGN KEY (`CID`) REFERENCES `classes` (`CID`),
   CONSTRAINT `FK_DID` FOREIGN KEY (`DID`) REFERENCES `division` (`DID`),
   CONSTRAINT `FK_UID` FOREIGN KEY (`UID`) REFERENCES `user` (`UID`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -242,8 +324,38 @@ CREATE TABLE `student` (
 
 LOCK TABLES `student` WRITE;
 /*!40000 ALTER TABLE `student` DISABLE KEYS */;
-INSERT INTO `student` VALUES (1,'PRATIK','JAIN',44,1,'Male','MH','PUNE','2019-12-19','pjpratikjin7@gmail,com','10TH','A',1,1,1,'pratik','123'),(2,'PRASAD','DESHMUKH',45,1,'Male','MH','PUNE','2019-12-18','pjprasaddeshmukh17@gmail,com','9TH','A',1,2,2,'prasad','123'),(3,'PRASHANT','JAIN',46,1,'Male','MH','PUNE','2019-12-05','pratikl17@gmail,com','10TH','A',1,2,3,'prashant','123'),(4,'PRATIK','LAVHANE',47,1,'Male','MH','PUNE','2019-11-29','PRATIK@gmail,com','8TH','A',1,3,4,'pratik1','123'),(5,'PRANAV','JOSHI',48,1,'Male','MH','PUNE','2019-12-10','pranav@gmail,com','10TH','A',1,1,5,'pranav','123'),(6,'SHRIPAD','JOSHI',49,1,'Male','MH','PUNE','2019-12-12','SHRIPAD@gmail,com','10TH','A',1,1,6,'shripad','123'),(7,'AKASH','PATIL',50,1,'Male','MH','PUNE','2019-12-07','chaitali@gmail,com','10TH','A',1,1,7,'akash','123'),(8,'CHAITALI','POTODE',51,1,'Female','MH','PUNE','2019-12-23','CHAI@gmail,com','8TH','A',1,3,8,'chaitali','123'),(18,'Shaurya','Jain',55,1,'Male','MH','Buldhana','2019-12-12','pjshaurya@gmail.com','10TH','A',1,1,19,'shaurya','123');
+INSERT INTO `student` VALUES (4,'PRATIK','LAVHANE',47,4,'Male','MH','PUNE','2019-11-28','PRATIK@gmail,com','8TH','A',1,3,4,'pratik1','123'),(5,'PRANAV','JAIN',48,5,'Male','MH','PUNE','2019-12-08','pranav@gmail,com','10TH','A',1,1,5,'pranav','123'),(6,'SHRIPAD','JOSHI',49,13,'Male','MH','PUNE','2019-12-11','SHRIPAD@gmail,com','10TH','A',1,1,6,'shripad','123'),(7,'AKASH','PATIL',50,7,'Male','MH','PUNE','2019-12-06','akashpatil@gmail,com','10TH','A',1,1,7,'akash','123'),(8,'CHAITALI','POTODE',51,15,'Female','MH','PUNE','2019-12-22','CHAI@gmail,com','8TH','A',1,3,8,'chaitali','123'),(23,'Vaishnavi','Patil',11,18,'Female','MH','Pune','2020-01-15','vp@gmail.com','10TH','A',1,1,26,'vaish11','123');
 /*!40000 ALTER TABLE `student` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tests`
+--
+
+DROP TABLE IF EXISTS `tests`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tests` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `test_name` varchar(500) DEFAULT NULL,
+  `class_name` varchar(52) DEFAULT NULL,
+  `class_id` int(11) DEFAULT NULL,
+  `date` date DEFAULT NULL,
+  `subject` varchar(15) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `class_id` (`class_id`),
+  CONSTRAINT `tests_ibfk_1` FOREIGN KEY (`class_id`) REFERENCES `classes` (`CID`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tests`
+--
+
+LOCK TABLES `tests` WRITE;
+/*!40000 ALTER TABLE `tests` DISABLE KEYS */;
+INSERT INTO `tests` VALUES (1,'Unit Test','9TH',2,'2020-01-15','Math'),(2,'Unit Test 2','9TH',2,'2020-01-12','Math'),(3,'Unit test 2','10TH',1,'2020-01-26','Science'),(4,'Unit 2','9TH',2,'2020-01-02','Hostory');
+/*!40000 ALTER TABLE `tests` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -261,7 +373,7 @@ CREATE TABLE `user` (
   `is_block` tinyint(1) DEFAULT NULL,
   `role` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`UID`)
-) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=38 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -270,7 +382,7 @@ CREATE TABLE `user` (
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES ('pratik@gmail.com','pratik','123',1,0,'student'),('prasad@gmail.com','prasad','123',2,0,'student'),('prashant@gmail.com','prashant','123',3,0,'student'),('pratik.1@gmail.com','pratik1','123',4,0,'student'),('pranav@gmail.com','pranav','123',5,0,'student'),('shripad@gmail.com','shripad','123',6,0,'student'),('akash@gmail.com','akash','123',7,0,'student'),('chaitali@gmail.com','chaitali','123',8,0,'student'),('rahul@gmail.com','rahul','123',9,0,'faculty'),('manjush@gmail.com','manjusha','123',10,0,'faculty'),('rohan@gmail.com','rohan','123',11,0,'faculty'),('shadha@gail.com','shradha','123',14,0,'student'),('neeta@gmail.com','neeta','123',15,0,'student'),('brijesh@gmail.com','Brijesh','123',16,0,'student'),('sandeep@gmail.com','sandeep','123',17,0,'faculty'),('prashant@gmail.com','prashant','123',18,0,'parent'),('pjshaurya@gmail.com','shaurya','123',19,0,'student');
+INSERT INTO `user` VALUES ('prashant@gmail.com','prashant','123',3,0,'student'),('pratik.1@gmail.com','pratik1','123',4,0,'student'),('pranav@gmail.com','pranav','123',5,0,'student'),('shripad@gmail.com','shripad','123',6,0,'student'),('akash@gmail.com','akash','123',7,0,'student'),('chaitali@gmail.com','chaitali','123',8,0,'student'),('rahul@gmail.com','rahul','123',9,0,'faculty'),('manjush@gmail.com','manjusha','123',10,0,'faculty'),('rohan@gmail.com','rohan','123',11,0,'faculty'),('shadha@gail.com','shradha','123',14,0,'student'),('neeta@gmail.com','neeta','123',15,0,'student'),('brijesh@gmail.com','Brijesh','123',16,0,'student'),('prashant@gmail.com','prashant','123',18,0,'parent'),('pjshaurya@gmail.com','shaurya','123',19,0,'student'),('niranjan@gmail.com','niranjan','123',22,0,'student'),('ram@gmail.com','ram','123',23,0,'student'),('raj@gmail.com','raj','123',25,0,'student'),('vp@gmail.com','vaish11','123',26,0,'student'),('prashant@gmail.com','prashantjain','123',27,0,'parent'),('sanjay@gmail.com','sanjay','123',28,0,'parent'),('bp@gmail.com','bob','123',29,0,'parent'),('pl@gmail.com','prabhakar','123',30,0,'parent'),('pravinjoshi@gmail.com','pravin','123',31,0,'parent'),('sham@gmail.com','sham','123',32,0,'parent'),('RAHUKPATIL@GMAIL.COM','rahul','123',33,0,'parent'),('dilippotode@gmail.com','diliip','123',34,0,'parent'),('rajjain@gmail.com','rajesh','123',35,0,'parent'),('kunal@gmail.com','kunal','123',37,0,'faculty');
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -283,4 +395,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-12-28 23:53:51
+-- Dump completed on 2020-01-29 12:54:22
